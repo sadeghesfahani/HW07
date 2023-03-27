fe3e84b4a16f1bac4ed0c8798e9256fe15e5a034
Sina <sadeghesfahani.sina@gmail.com>
2023-03-27 11:40:50 +0430

    Implement database connection pool

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+    static HikariConfig config = new HikariConfig();
+    public static void main(String[] args) {
+
+        config.setJdbcUrl("jdbc:postgresql://localhost:5456/db_legal");
+        config.setUsername("admin");
+        config.setPassword("admin");
+
+        DataSource dataSource = new HikariDataSource(config);
+        try (Connection conn = dataSource.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM legal")) {
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
